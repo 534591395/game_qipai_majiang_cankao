8d8fbb0916aba22965a6d30973666a06156e0b1d
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-04-30 17:06:05 +0800

    【】

--- server/start_all.sh
+++ server/start_all.sh
@@ -1,3 +1,3 @@
-nohup node ./account_server/app.js ../configs.js &
-nohup node ./hall_server/app.js ../configs.js &
-nohup node ./game_server/app.js ../configs.js &
+nohup node ./account_server/app.js ../configs_win.js &
+nohup node ./hall_server/app.js ../configs_win.js &
+nohup node ./game_server/app.js ../configs_win.js &
